ee4c75626c59e281884ca8ccd9b25cb0bf4e888a
Vladimir-Siarov-EPAM <Vladimir_Siarov@epam.com>
2019-07-15 21:25:54 +0300

    End chapter 3.1

---
+++ quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Models.Question question)
+        {
+
+        }
+    }
+}
